0a04d59b3006bed2902edc82922641d0eda3ed55
Xinco <nicola475@yahoo.it>
2018-11-19 10:51:58 +0100

    Problema risolto

---
+++ app/controllers/pagesrails_controller.rb
@@ -0,0 +1,37 @@
+class PagesrailsController < ApplicationController
+  def generate
+  end
+
+  def controller
+  end
+
+  def pagesrails
+  end
+
+  def generate
+  end
+
+  def controller
+  end
+
+  def pagesrails
+  end
+
+  def generate
+  end
+
+  def controller
+  end
+
+  def pagesrails
+  end
+
+  def generate
+  end
+
+  def controller
+  end
+
+  def pages
+  end
+end
---
+++ test/controllers/pagesrails_controller_test.rb
@@ -0,0 +1,64 @@
+require 'test_helper'
+
+class PagesrailsControllerTest < ActionDispatch::IntegrationTest
+  test "should get generate" do
+    get pagesrails_generate_url
+    assert_response :success
+  end
+
+  test "should get controller" do
+    get pagesrails_controller_url
+    assert_response :success
+  end
+
+  test "should get pagesrails" do
+    get pagesrails_pagesrails_url
+    assert_response :success
+  end
+
+  test "should get generate" do
+    get pagesrails_generate_url
+    assert_response :success
+  end
+
+  test "should get controller" do
+    get pagesrails_controller_url
+    assert_response :success
+  end
+
+  test "should get pagesrails" do
+    get pagesrails_pagesrails_url
+    assert_response :success
+  end
+
+  test "should get generate" do
+    get pagesrails_generate_url
+    assert_response :success
+  end
+
+  test "should get controller" do
+    get pagesrails_controller_url
+    assert_response :success
+  end
+
+  test "should get pagesrails" do
+    get pagesrails_pagesrails_url
+    assert_response :success
+  end
+
+  test "should get generate" do
+    get pagesrails_generate_url
+    assert_response :success
+  end
+
+  test "should get controller" do
+    get pagesrails_controller_url
+    assert_response :success
+  end
+
+  test "should get pages" do
+    get pagesrails_pages_url
+    assert_response :success
+  end
+
+end
