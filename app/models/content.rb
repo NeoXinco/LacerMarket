69b45b0680fcb27dc31b3f71fb63497ce76b9f93
Xinco <nicola475@yahoo.it>
2018-12-07 17:46:37 +0100

    modificato il design di index e show

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,12 +1,12 @@
 class Content < ApplicationRecord
 	belongs_to :user
 
-	has_attached_file :cover, styles: { medium: "300x>", thumb: "100x>" }
+	has_attached_file :cover
   	validates_attachment_content_type :cover, content_type: /\Aimage\/.*\z/,
   	message: "Solo le immagini sono supportate"
 
   	has_attached_file :allegato
-  	validates_attachment_content_type :allegato, :content_type => [ /^image\/(png|gif|jpeg)/, 'application/vnd.openxmlformats-officedocument.wordprocessingml. document','application/mspowerpoint','application/vnd.ms-powerpoint','applicaton/vnd.openxmlformats-officedocument.presentationml.presentaion','application/pdf','applicaton/msexcel', 'application/vnd.ms-excel','applicaton/vnd.openxmlformats-officedocument.spreadshhetml.sheet','audio/mpeg','adio/mp3'] , 
+  	validates_attachment_content_type :allegato, :content_type => [ /^image\/(png|gif|jpeg)/, 'application/vnd.openxmlformats-officedocument.wordprocessingml.document','application/mspowerpoint','application/vnd.ms-powerpoint','applicaton/vnd.openxmlformats-officedocument.presentationml.presentaion','application/pdf','applicaton/msexcel', 'application/vnd.ms-excel','applicaton/vnd.openxmlformats-officedocument.spreadsheetml.sheet','audio/mpeg','audio/mp3'] , 
   	message: "Formato non supportato"
  
 end
