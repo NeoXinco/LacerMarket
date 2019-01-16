6633ab7b3021459f4b849deba4d25da96dd1acea
Xinco <nicola475@yahoo.it>
2019-01-16 19:06:28 +0100

    installate gem per heroku

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,5 @@ module LacerMarket
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :it
-
-    config.assets.initialize_on_precompile = false
   end
 end
