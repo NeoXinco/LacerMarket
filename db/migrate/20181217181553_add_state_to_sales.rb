4fdb1799902faf5e23aeaa0ff22740d165ac7f3a
Xinco <nicola475@yahoo.it>
2019-01-16 17:17:28 +0100

    internazionalizzazione e Flash messages

--- db/migrate/20181217181553_add_state_to_sales.rb
+++ db/migrate/20181217181553_add_state_to_sales.rb
@@ -1,6 +1,6 @@
 class AddStateToSales < ActiveRecord::Migration[5.1]
   def change
-    add_column :sales, :state, :tring
+    add_column :sales, :state, :string
     add_column :sales, :stripe_id, :string
     add_column :sales, :stripe_token, :string
     add_column :sales, :error, :text
