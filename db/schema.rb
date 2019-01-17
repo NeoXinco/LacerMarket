f230fce070d7e99ab006ac49ceca4f1535c3cc49
Xinco <nicola475@yahoo.it>
2019-01-17 18:46:52 +0100

    installate gem per heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181217222713) do
+ActiveRecord::Schema.define(version: 2018_12_17_222713) do
 
   create_table "contents", force: :cascade do |t|
     t.string "titolo"
