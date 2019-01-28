5c969133505889c840a37b87c433eece04868e53
Xinco <nicola475@yahoo.it>
2019-01-28 19:29:00 +0100

    Dashboard e tocchi finali

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,11 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.cache_classes = true
+
+  # Store files on Amazon S3.
+  config.active_storage.service = :amazon
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
@@ -98,6 +103,3 @@ Rails.application.configure do
     }
   } 
 end
-
-  # Store files on Amazon S3.
-  config.active_storage.service = :amazon
